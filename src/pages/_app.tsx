b19783e93c27d4f5a5bda1c22790b40b7946ca3c
By_Ha <By_Ha@qq.com>
2023-04-21 02:35:45 +0800

    feat: update toolbar and calendar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import { NextPage } from 'next';
 import React, { ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import { ConfigProvider, theme } from 'antd';
+import dayjs from 'dayjs';
 import zhCN from 'antd/lib/locale/zh_CN';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
 import NoSsr from '@/components/NoSsr';
 import antdTheme from '@/styles/antdTheme.json';
 import { DefaultLayout } from '@/layout/DefaultLayout';
 
 import '@/styles/globals.scss';
 import 'tailwindcss/tailwind.css';
+
+dayjs.extend(isLeapYear);
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
